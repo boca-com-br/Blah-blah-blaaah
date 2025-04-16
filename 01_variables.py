41be1c2ecce9524474fe56a7bc3776fe9592780b
boca-com-br <romanemilianoprietoroldan@gmail.com>
2025-04-16 09:10:20 -0300

    01_variables.py
    Modificacion de Nombre, Apellido, Alias y Edad

--- 01_variables.py
+++ 01_variables.py
@@ -23,7 +23,7 @@ print("Este es el valor de:", my_bool_variable)
 print(len(my_string_variable))
 
 # Variables en una sola línea. ¡Cuidado con abusar de esta sintaxis!
-name, surname, alias, age = "Brais", "Moure", 'MoureDev', 35
+name, surname, alias, age = "Roman", "Roldan", 'Emilio', 17
 print("Me llamo:", name, surname, ". Mi edad es:",
       age, ". Y mi alias es:", alias)
 
@@ -34,8 +34,8 @@ print(name)
 print(age)
 
 # Cambiamos su tipo
-name = 35
-age = "Brais"
+name = 17
+age = "Roman"
 print(name)
 print(age)
 
