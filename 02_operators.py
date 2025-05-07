adcd774e1f0849233ec2e9afc6d41869f3d09d10
boca-com-br <romanemilianoprietoroldan@gmail.com>
2025-05-07 07:56:55 -0300

    02_operators.py.MOD
    Palabra modificada hola por HALLO

--- 02_operators.py
+++ 02_operators.py
@@ -13,15 +13,15 @@ print(2 ** 3)
 print(2 ** 3 + 3 - 7 / 1 // 4)
 
 # Operaciones con cadenas de texto
-print("Hola " + "Python " + "¿Qué tal?")
-print("Hola " + str(5))
+print("HALLO " + "Python " + "¿Qué tal?")
+print("HALLO " + str(5))
 
 # Operaciones mixtas
-print("Hola " * 5)
-print("Hola " * (2 ** 3))
+print("HALLO " * 5)
+print("HALLO " * (2 ** 3))
 
 my_float = 2.5 * 2
-print("Hola " * int(my_float))
+print("HALLO " * int(my_float))
 
 ### Operadores Comparativos ###
 
@@ -34,20 +34,20 @@ print(3 == 4)
 print(3 != 4)
 
 # Operaciones con cadenas de texto
-print("Hola" > "Python")
-print("Hola" < "Python")
+print("HALLO" > "Python")
+print("HALLO" < "Python")
 print("aaaa" >= "abaa")  # Ordenación alfabética por ASCII
 print(len("aaaa") >= len("abaa"))  # Cuenta caracteres
-print("Hola" <= "Python")
-print("Hola" == "Hola")
-print("Hola" != "Python")
+print("HALLO" <= "Python")
+print("HALLO" == "HOLA")
+print("HALLO" != "Python")
 
 ### Operadores Lógicos ###
 
 # Basada en el Álgebra de Boole https://es.wikipedia.org/wiki/%C3%81lgebra_de_Boole
-print(3 > 4 and "Hola" > "Python")
-print(3 > 4 or "Hola" > "Python")
-print(3 < 4 and "Hola" < "Python")
-print(3 < 4 or "Hola" > "Python")
-print(3 < 4 or ("Hola" > "Python" and 4 == 4))
+print(3 > 4 and "HALLO" > "Python")
+print(3 > 4 or "HALLO" > "Python")
+print(3 < 4 and "HALLO" < "Python")
+print(3 < 4 or "HALLO" > "Python")
+print(3 < 4 or ("HALLO" > "Python" and 4 == 4))
 print(not (3 > 4))
